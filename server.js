77c57e322ec77a53ba5750a3e88d0b4c63d5b782
powerar <arpower@bell.net>
2021-05-25 15:29:31 -0400

    added homepage route and render

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.set('view engine', 'handlebars');
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
-// app.use(require('./controllers'));
+app.use(require('./controllers'));
 
 sequelize.sync({ force: false }).then(() => {
     app.listen(PORT, () => console.log(`Now listening on port ${PORT}`));
