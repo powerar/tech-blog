bee53a359d835d0626c32797aeef07882feab94d
powerar <arpower@bell.net>
2021-05-26 15:28:03 -0400

    added login route, password hash, and password validation

--- controllers/index.js
+++ controllers/index.js
@@ -1,6 +1,11 @@
 const router = require('express').Router();
-const homeRoutes = require('./home-routes');
 
-router.use('/', homeRoutes);
+const apiRoutes = require('./api');
+
+router.use('/api', apiRoutes);
+
+router.use((req, res) => {
+  res.status(404).end();
+});
 
 module.exports = router;
