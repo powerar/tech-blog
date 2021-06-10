ec0b07438800003d0780c1d275f155435b0ac074
powerar <arpower@bell.net>
2021-06-10 19:03:23 -0400

    added Post routes

--- models/index.js
+++ models/index.js
@@ -1,4 +1,14 @@
 const User = require('./User');
 const Post = require('./Post');
 
+//create the relationship between Users and Posts
+User.hasMany(Post, {
+    foreignKey: 'user_id'
+});
+
+//create the relationship between Posts and Users
+Post.belongsTo(User, {
+    foreignKey: 'user_id',
+});
+
 module.exports = { User, Post };
