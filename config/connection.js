f6da0d7869279ee16d206e5d5bb8c49a71a56f17
powerar <arpower@bell.net>
2021-06-10 20:35:37 -0400

    connect to jawsdb

--- config/connection.js
+++ config/connection.js
@@ -4,12 +4,14 @@ require('dotenv').config({ path: path.resolve(__dirname, '../.env') });
 
 const Sequelize = require('sequelize');
 
-const sequelize = process.env.JAWSDB_URL
-    ? new Sequelize(process.env.JAWSDB_URL)
-    : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
-        host: 'localhost',
-        dialect: 'mysql',
-        port: 3306
-    });
+let sequelize;
 
-module.exports = sequelize;
+if (process.env.JAWSDB_URL) {
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+  sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+    host: 'localhost',
+    dialect: 'mysql',
+    port: 3306
+  });
+}
