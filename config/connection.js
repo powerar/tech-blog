60f191c21540b217148577c3c2423346d0f7780f
powerar <arpower@bell.net>
2021-06-12 17:42:46 -0400

    changed connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,17 +1,17 @@
+const Sequelize = require( 'sequelize' );
 const path = require('path');
-
 require('dotenv').config({ path: path.resolve(__dirname, '../.env') });
-
-const Sequelize = require('sequelize');
 
 let sequelize;
 
 if (process.env.JAWSDB_URL) {
-  sequelize = new Sequelize(process.env.JAWSDB_URL);
+  sequelize = new Sequelize( process.env.JAWSDB_URL );
 } else {
-  sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+  sequelize = new Sequelize( process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
     host: 'localhost',
     dialect: 'mysql',
     port: 3306
-  });
+  } );
 }
+
+module.exports = sequelize;
